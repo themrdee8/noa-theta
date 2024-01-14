b863bc409fecdc1e63e8481e7fc9068212f1e985
Senyo Dedoo <themrdee@Senyos-MacBook-Air.local>
2024-01-14 13:54:34 +0000

    Second COmmit

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+import axios from "axios";
+import * as z from "zod";
+import Header from "@/components/Header";
+import { Video } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Empty } from "@/components/Empty";
+import { Loading } from "@/components/Loading";
+import { cn } from "@/lib/utils";
+
+interface ChatCompletionRequestMessageParam {
+  role: string;
+  content: string;
+}
+
+const VideoPage = () => {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+
+      const response = await axios.post("/api/video", values);
+
+      setVideo(response.data[0]);
+      form.reset();
+    } catch (error: any) {
+      // TODO: Open Pro model
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title="Video Generation"
+        description="Generate videos with Noa"
+        icon={Video}
+        iconColor="text-orange-600"
+        bgColor="bg-orange-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg w-full border p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="col-span-12 lg:col-span-10">
+                      <FormControl className="m-0 p-0">
+                        <Input
+                          className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                          disabled={isLoading}
+                          placeholder="A lion hunting"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  );
+                }}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loading />
+            </div>
+          )}
+          {!video && !isLoading && <Empty label={"How may I assist you?"} />}
+          {video && (
+            <video
+              className="w-full aspect-video border bg-black mt-8 rounded-lg"
+              controls
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage;
